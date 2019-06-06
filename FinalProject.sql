b82714fd200ca2428429785bfb56e73c3fe5c79a
robermancera <48019891+robermancera@users.noreply.github.com>
2019-06-06 08:25:22 -0500

    Add files via upload

---
+++ FinalProject.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_cobranza;
+
+USE db_cobranza;
+
+CREATE TABLE tbl_usuario(fld_id INT(3), fld_nombre VARCHAR(20), fld_telefono INT(10), 
+fld_correo VARCHAR(40), fld_password VARCHAR(25), fld_propietario BOOLEAN, primary key(fld_id));
+
+CREATE TABLE tbl_pago(fld_id INT(3), fld_fecha date, fld_cantidad int(20), fld_id_usuario int(20), primary key(fld_id));
+
+CREATE TABLE tbl_deudas(fld_id INT(3), fld_fecha date, fld_cantidad int(20), fld_id_usuario int(20), primary key(fld_id));
+
