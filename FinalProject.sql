10caeab423b5c38d3946aadd296d2cdd5444b690
robermancera <48019891+robermancera@users.noreply.github.com>
2019-06-06 08:36:19 -0500

    Agregar fld_concepto
    A la tabla deuda

--- FinalProject.sql
+++ FinalProject.sql
@@ -7,5 +7,5 @@ fld_correo VARCHAR(40), fld_password VARCHAR(25), fld_propietario BOOLEAN, prima
 
 CREATE TABLE tbl_pago(fld_id INT(3), fld_fecha date, fld_cantidad int(20), fld_id_usuario int(20), primary key(fld_id));
 
-CREATE TABLE tbl_deudas(fld_id INT(3), fld_fecha date, fld_cantidad int(20), fld_id_usuario int(20), primary key(fld_id));
+CREATE TABLE tbl_deudas(fld_id INT(3), fld_fecha date, fld_cantidad int(20),fld_concepto VARCHAR(255), fld_id_usuario int(20), primary key(fld_id));
 
